a5eb6135602d2a5b4f684703db7d7c0aa1fbeb31
Yann B <57952703+YannBol@users.noreply.github.com>
2025-11-13 13:20:39 +0100

    Update fichier_equipe.py

--- fichier_equipe.py
+++ fichier_equipe.py
@@ -3,16 +3,13 @@ import pandas as pd
 import time
 from sqlalchemy import create_engine
 
-# ⚡ PostgreSQL
 engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/football")
 
-# API Football
 API_KEY = "79df369c68fa417296dfdb3a5986ef6d"
 BASE_URL = "https://api.football-data.org/v4"
 headers = {"X-Auth-Token": API_KEY}
 
-# Championnats
-competitions = ["PL", "FL1", "BL1", "SA", "PD"]  # Premier League, Ligue 1, Bundesliga, Serie A, La Liga
+competitions = ["PL", "FL1", "BL1", "SA", "PD"] 
 
 teams_data = []
 
@@ -30,16 +27,13 @@ for comp in competitions:
             "venue": team.get("venue")
         })
 
-    # Pause obligatoire pour l'API gratuite (10 appels/min)
     time.sleep(6)
 
-# Mettre en DataFrame
 df_teams = pd.DataFrame(teams_data)
 
-# Supprimer espaces ou caractères parasites dans les noms de colonnes
 df_teams.columns = df_teams.columns.str.strip()
 
-# Insérer dans PostgreSQL dans la table 'teams' (replace)
 df_teams.to_sql("teams", engine, if_exists="replace", index=False)
 
 print(f"✅ Toutes les équipes ont été importées dans la table 'teams' ({len(df_teams)} lignes)")
+
